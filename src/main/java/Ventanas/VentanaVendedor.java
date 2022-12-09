429c6644b883f6a204f8fe428247e9f957cde05b
JoseAntonioMardonez <joseantoniomar98@gmail.com>
2022-12-09 03:44:13 -0300

    Se sube version 09-12-22

--- src/main/java/Ventanas/VentanaVendedor.java
+++ src/main/java/Ventanas/VentanaVendedor.java
@@ -3,11 +3,13 @@ package Ventanas;
 import Tienda.Tienda;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class VentanaVendedor extends Ventana{
     private JButton botonMostrarInventario;
     private JButton botonBuscarProducto;
-    private JButton botonAgregarProducto;
+    private JButton botonVenta;
+    private JButton botonVolver;
     private Tienda tienda;
 
     public VentanaVendedor(Tienda tienda) {
@@ -17,8 +19,50 @@ public class VentanaVendedor extends Ventana{
     }
 
     private void generarElementosVentana() {
-        //generarBoton();
-        //generarBoton();
-        //generarBoton();
+        generarBotonMostrarInventario();
+        generarBotonBuscarProducto();
+        generarBotonVenta();
+        generarBotonVolver();
+    }
+    private void generarBotonMostrarInventario() {
+        String textoBoton = "Mostrar Inventario";
+        this.botonMostrarInventario = super.generarBoton(textoBoton, 175, 100, 150, 40);
+        this.add(this.botonMostrarInventario);
+        this.botonMostrarInventario.addActionListener(this);
+    }
+    private void generarBotonBuscarProducto() {
+        String textoBoton = "Buscar Producto";
+        this.botonBuscarProducto = super.generarBoton(textoBoton, 175, 180, 150, 40);
+        this.add(this.botonBuscarProducto);
+        this.botonBuscarProducto.addActionListener(this);
+    }
+    private void generarBotonVenta() {
+        String textoBoton = "Venta";
+        this.botonVenta = super.generarBoton(textoBoton, 175, 260, 150, 40);
+        this.add(this.botonVenta);
+        this.botonVenta.addActionListener(this);
+    }
+    private void generarBotonVolver() {
+        String textoBoton = "Volver";
+        this.botonVolver = super.generarBoton(textoBoton, 175, 380, 150, 40);
+        this.add(this.botonVolver);
+        this.botonVolver.addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.botonMostrarInventario) {
+            VentanaInventario inventario = new VentanaInventario(tienda);
+            inventario.setLocationRelativeTo(null);
+            inventario.setVisible(true);
+        }
+        if (e.getSource() == this.botonBuscarProducto) {
+            VentanaBuscarProducto buscarProducto = new VentanaBuscarProducto(tienda);
+            //Cierra la ventana actual
+            this.dispose();
+        }
+        if (e.getSource() == this.botonVolver) {
+            new VentanaPrincipal(tienda);
+            //Cierra la ventana actual
+            this.dispose();
+        }
     }
 }
